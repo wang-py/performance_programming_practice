3816d4c1b7287c7d4e4b32cebd857cb763bc44b2
Panyue Wang <pywang@ucdavis.edu>
2024-07-21 16:42:40 -0700

    added buffer

--- 8086_disassembler.c
+++ 8086_disassembler.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+unsigned char* initialize_buffer(unsigned char* buffer);
+void delete_buffer(unsigned char* buffer);
+
+unsigned char* initialize_buffer(unsigned char* buffer) {
+    buffer = (unsigned char*)malloc(sizeof(unsigned char) * 32);
+    return buffer;
+}
+
+void delete_buffer(unsigned char* buffer) {
+    free(buffer);
+}
+
+int main(int argv, char* argc[]) {
+    printf("number of arguments: %d\n", argv);
+    printf("arguments:\n");
+    for (int i = 0; i < argv; i++) {
+        printf("%s ", argc[i]);
+    }
+    printf("\n");
+
+    unsigned char* buffer;
+    printf("initializing buffer...\n");
+    buffer = initialize_buffer(buffer);
+    printf("deleting buffer...\n");
+    delete_buffer(buffer);
     
     return 0;
 }
