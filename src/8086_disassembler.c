7f26111fd4e65f5c8fa629a8b192a785dc1ae2d9
Panyue Wang <pywang@ucdavis.edu>
2024-12-27 12:31:33 -0800

    adding cmp add sub

--- src/8086_disassembler.c
+++ src/8086_disassembler.c
@@ -13,6 +13,8 @@ int REG_IMMEDIATE_MASK = 7;
 int IMMEDIATE_MODE = 1;
 int MOD_BITS = 3;
 
+const char* op[8] = {"add", "", "", "", "", "sub", "", "cmp"};
+
 const char* reg_list_wide[8] = {"ax", "cx", "dx", "bx",
                                 "sp", "bp" , "si", "di"};
 
@@ -29,6 +31,7 @@ unsigned char* initialize_buffer(unsigned char* buffer, int buffer_size);
 int is_wide(unsigned char byte_1);
 int is_wide_immediate(unsigned char byte_1);
 int get_direction_bit(unsigned char instruction);
+void decode_non_mov_op(unsigned char op);
 void decode_reg_field(unsigned char reg, int width);
 void decode_reg(unsigned char reg);
 void decode_reg_wide(unsigned char reg);
@@ -77,6 +80,10 @@ int decode_byte_1(unsigned char byte_1) {
         case 14:
             printf("cmp ");
             break;
+        case 32:
+            // printf("cmp ");
+            retval = IMMEDIATE_MODE;
+            break;
         if ((byte_1 & WIDTH_BIT_IMMEDIATE) == WIDTH_BIT_IMMEDIATE) {
             decode_reg_wide(byte_1 & REG_IMMEDIATE_MASK);
         } else {
@@ -95,6 +102,10 @@ int decode_byte_1(unsigned char byte_1) {
     */
 
     return retval;
+}
+
+void decode_non_mov_op(unsigned char reg) {
+    printf("%s", op[reg]);
 }
 
 void decode_reg_wide(unsigned char reg) {
@@ -222,6 +233,7 @@ void decode_assembly(unsigned char* buffer, int inst_size) {
     int width_immediate = 0;
     unsigned char disp_mode = 0;
     int d = 0;
+    unsigned char reg = 0;
     // direction of mov
     while (i < inst_size) {
         // printf("%x %x\n", buffer[i], buffer[i + 1]);
@@ -231,6 +243,8 @@ void decode_assembly(unsigned char* buffer, int inst_size) {
 
         if (mode == IMMEDIATE_MODE) {
             width_immediate = is_wide_immediate(buffer[i]);
+            reg = (*(buffer + i + 1) >> 3) & REG_BITS;
+            decode_non_mov_op(reg);
             decode_byte_2_i(buffer + i + 1, width_immediate);
             if (width_immediate == 1) {
                 i += 3;
