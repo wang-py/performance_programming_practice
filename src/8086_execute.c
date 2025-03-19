8e819f8da2bba9580e1f2af93c2414e5eabdd50b
Panyue Wang <pywang@ucdavis.edu>
2025-03-19 12:18:58 -0700

    redirect disassembler output

--- src/8086_execute.c
+++ src/8086_execute.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
@@ -6,7 +7,7 @@
 
 static char* sub_process_name = "../sim86/sim86";
 
-int call_disassembler(char* argv[]) {
+int disassemble_code(char* argv[], char* disassembled_code) {
     int pipefd[2];
     pipe(pipefd);
     int pid = fork();
@@ -34,14 +35,17 @@ int call_disassembler(char* argv[]) {
     close(pipefd[1]);
 
     while (read(pipefd[0], buffer, sizeof(buffer)) != 0) {
-        printf(buffer);
+        strcpy(disassembled_code, buffer);
     }
 
-    printf("All children have finished.\n");
+    // printf("All children have finished.\n");
     return 0;
 }
 
 int main(int argc, char* argv[]) {
-    call_disassembler(argv);
+    char* disassembled_code = malloc(sizeof(char) * 1024);
+    disassemble_code(argv, disassembled_code);
+    printf(disassembled_code);
+    free(disassembled_code);
     return 0;
 }
