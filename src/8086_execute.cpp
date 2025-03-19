8972eca0f93ac5c2de8ca8c6c798df33bf5cec65
Panyue Wang <pywang@ucdavis.edu>
2025-03-19 12:41:15 -0700

    linking library

--- src/8086_execute.cpp
+++ src/8086_execute.cpp
@@ -1,9 +1,14 @@
 #include "sim86_shared.h"
 #include <iostream>
 
-int main(void) {
-
-    std::cout << "hello world" << std::endl;
+int main(int argc, char* argv[]) {
+    u32 Version = Sim86_GetVersion();
+    printf("Sim86 Version: %u (expected %u)\n", Version, SIM86_VERSION);
+    if(Version != SIM86_VERSION)
+    {
+        printf("ERROR: Header file version doesn't match DLL.\n");
+        return -1;
+    }
 
     return 0;
 }
